bc888d1ff1bcaf49c30af78337e4c35e7643c2c0
OM JI DUBEY <omjeed0666@gmail.com>
2025-03-11 01:03:11 +0530

    Time: 1 ms (99.18%), Space: 41.3 MB (94.58%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,21 +1,23 @@
 class Solution {
     public int maxScore(String s) {
-        int max = 0;
-        for(int i = 0 ; i< s.length()-1;i++){
-            int count_0 = 0;
-            int count_1 = 0;
-            for(int j = 0; j <= i ; j++){
-                if(s.charAt(j) == '0'){
-                    count_0++;
-                }
+        int max = 0 ;
+        int pre= 0;
+        int suf = 0;
+        for(int i = 0 ; i< s.length();i++){
+            if(s.charAt(i) == '0'){
+                pre++;
             }
-            for(int j =i+1; j< s.length();j++){
-                if(s.charAt(j) == '1'){
-                    count_1++;
-                }
+        }
+        for(int i = s.length()-1; i>0;i--){
+            if(s.charAt(i) == '1'){
+                suf++;
+                int sum = suf+pre;
+                max = Math.max(max,sum);
+            }else{
+                pre--;
+                int sum = suf+pre;
+                max = Math.max(max,sum);
             }
-            int sum = count_0+ count_1;
-            max = Math.max(max,sum);
         }
         return max;
     }
