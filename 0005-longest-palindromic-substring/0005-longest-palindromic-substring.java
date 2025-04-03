ff90bc30aba87fb0f9288bce8b9d6203ed776006
OM JI DUBEY <omjeed0666@gmail.com>
2025-04-03 10:27:19 +0530

    Time: 18 ms (63.16%), Space: 42.8 MB (61.53%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,39 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        String maxStr = s.substring(0, 1);
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            String odd = expandFromCenter(s, i, i);
-            String even = expandFromCenter(s, i, i + 1);
-
-            if (odd.length() > maxStr.length()) {
-                maxStr = odd;
+        int max = 0;
+        int left =0,right = 0;
+        int i = 0;
+        //ODD
+        while(i<s.length()-1){
+            int j = i-1,k=i+1;
+            while(j>=0 && k <s.length() && s.charAt(j) == s.charAt(k)){
+                if(k-j+1 > max){
+                    left = j;
+                    right = k;
+                    max = k-j+1;
+                }
+                j--;
+                k++;
             }
-            if (even.length() > maxStr.length()) {
-                maxStr = even;
+            i++;
+        }
+
+        //EVEN
+        i=0;
+        while(i<s.length()-1){
+            int j = i,k=i+1;
+            while(j>=0 && k <s.length() && s.charAt(j) == s.charAt(k)){
+                if(k-j+1 > max){
+                    left = j;
+                    right = k;
+                    max = k-j+1;
+                }
+                j--;
+                k++;
             }
+            i++;
         }
 
-        return maxStr;
-    }
-
-    private String expandFromCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return s.substring(left + 1, right);
+        return s.substring(left,right+1);
     }
 }
