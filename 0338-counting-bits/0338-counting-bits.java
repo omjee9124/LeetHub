6652ac4c608b97f4a3ca747cb80a042f2643c284
OM JI DUBEY <omjeed0666@gmail.com>
2025-04-07 09:02:30 +0530

    Time: 2 ms (94.91%), Space: 46.6 MB (83.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int arr[] = new int[n+1];
         arr[0] = 0;
         for(int i = 0; i<=n;i++){
-            arr[i] = (i%2) + arr[i/2];
+            arr[i] = (i&1) + arr[i>>1];
         }
         return arr;
     }
