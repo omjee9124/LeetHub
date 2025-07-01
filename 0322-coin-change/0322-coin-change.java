817bf884264bd32f587a2f4f8b7d3aad076178f6
OM JI DUBEY <omjeed0666@gmail.com>
2025-07-02 03:20:44 +0530

    Time: 15 ms (78.32%), Space: 44.8 MB (47.79%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,34 +1,36 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int arr[][] = new int[coins.length][amount+1];
-        for(int i = 0 ; i < coins.length;i++){
-            for(int j = 0; j<= amount; j++){
-                arr[i][j] = Integer.MAX_VALUE -1;
-            }
+        int n = coins.length;
+        int t[][] = new int[n+1][amount+1];
+
+        //Initialization
+
+        for(int i = 0 ; i <= amount; i++){
+            t[0][i] = Integer.MAX_VALUE-1;
+
         }
-        for(int i = 0; i<=amount; i++){
-            if(i % coins[0] == 0){
-                arr[0][i] = i/coins[0];
+        for(int i = 1 ; i <= amount; i++){
+            if(i%coins[0] ==0){
+                t[1][i] = i/coins[0];
+            }else{
+                t[1][i] = Integer.MAX_VALUE-1;
             }
-             
+            
+
         }
-        for(int i = 1;i< coins.length; i++){
-            for(int j =0; j <= amount; j++){
-                if(arr[i][j] == Integer.MAX_VALUE -1){
-                    if(coins[i] > j){
-                    arr[i][j] = arr[i-1][j];
-                        }else{
-                            arr[i][j] = Math.min(arr[i-1][j],1+arr[i][j-coins[i]]);
-                        }
+        for(int i = 1; i<=n; i++){
+            t[i][0] = 0;
+        }
+
+        for(int i = 2; i<=n; i++){
+            for(int j = 1; j<=amount; j++){
+                if(coins[i-1] <=j){
+                    t[i][j] = Math.min(1 + t[i][j-coins[i-1]],t[i-1][j]);
+                }else{
+                    t[i][j] = t[i-1][j];
                 }
-                
             }
         }
-        int res = arr[coins.length-1][amount];
-        if(res == Integer.MAX_VALUE-1){
-            return -1;
-        }else{
-            return res;
-        }
+        return t[n][amount] == Integer.MAX_VALUE - 1 ? -1 : t[n][amount];
     }
 }
