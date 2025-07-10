c8b636df179abb11ddcf61d79b640d1c51ae68d1
OM JI DUBEY <omjeed0666@gmail.com>
2025-07-11 03:24:18 +0530

    Time: 4 ms (5.17%), Space: 42.9 MB (5.28%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSubsequence(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        
+        
+        int t[][] = new int[m+1][n+1];
+        
+        for(int i =0 ; i<=m; i++){
+            for(int j =0; j<=n; j++){
+                if(i==0 || j ==0){
+                    t[i][j] = 0;
+                }
+            }
+        }
+        
+        for(int i =1; i<=m; i++){
+            for(int j =1; j<=n; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    t[i][j] = 1+t[i-1][j-1];
+                }else{
+                    t[i][j] = Math.max(t[i][j-1],t[i-1][j]);
+                }
+            }
+        }
+        return m == t[m][n];
+    }
+}
