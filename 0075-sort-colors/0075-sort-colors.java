cee773e1e5157d84294936c21f411b702befd9bd
Om Ji Dubey <omjeed0666@gmail.com>
2026-06-28 21:34:56 +0530

    Time: 3 ms (13.79%), Space: 43.8 MB (10.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-        //Best way - in single pass
-        int low = 0;
-        int mid = 0;
-        int high = nums.length-1;
-        while(mid<=high){
-            if(nums[mid] == 2){
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high--;
-            }else if(nums[mid] ==0){
-                int temp = nums[mid];
-                nums[mid] = nums[low];
-                nums[low] = temp;
-                low++;
-                mid++;
-            }else{
-                mid++;
-            }
-        }
-
+        // Brute Force
+        Arrays.sort(nums);
     }
 }
