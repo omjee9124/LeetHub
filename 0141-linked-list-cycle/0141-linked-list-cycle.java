7ada6c9cb5e8ecf276f3e66e8bca2e0e588db4f9
Om Ji Dubey <omjeed0666@gmail.com>
2026-07-11 00:26:51 +0530

    Time: 0 ms (100%), Space: 46.6 MB (72.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        if(head == null){
-            return false;
-        }
-        while(fast!=null && fast.next!=null){
-            
-            fast = fast.next.next;
-            slow = slow.next;
-            if(slow == fast) return true;
-        }
-        return false;
+        if(head == null ) return false;
+       ListNode slow = head;
+       ListNode fast = head;
+       while(fast!=null && fast.next!= null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast) return true;
+       }
+       return false;
     }
 }
