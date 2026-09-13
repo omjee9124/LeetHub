a2e97bbb4d25b8346387e4a37016c791bfbd6eee
Om Ji Dubey <omjeed0666@gmail.com>
2026-09-13 15:53:40 +0530

    Time: 3 ms (47.06%), Space: 46.2 MB (81.76%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,79 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int boxIndex = (i / 3) * 3 + (j / 3);
 
-                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
-                        return false;
-                    }
+                boolean A = isRowValid(i, j, board);
+                boolean B = isColValid(i, j, board);
 
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+                int C = (i / 3) * 3;
+                int D = (j / 3) * 3;
+
+                boolean E = isBoxValid(i, j, C, D, board);
+
+                if (!A || !B || !E) {
+                    return false;
                 }
             }
         }
+
+        return true;
+    }
+
+    boolean isRowValid(int i, int j, char[][] board) {
+
+        int count = 0;
+
+        for (int k = 0; k < 9; k++) {
+
+            if (board[i][k] == board[i][j] && board[i][k] != '.') {
+                count++;
+
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    boolean isColValid(int i, int j, char[][] board) {
+
+        int count = 0;
+
+        for (int k = 0; k < 9; k++) {
+
+            if (board[k][j] == board[i][j] && board[k][j] != '.') {
+                count++;
+
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    boolean isBoxValid(int i, int j, int C, int D, char[][] board) {
+
+        int count = 0;
+
+        for (int k = C; k < C + 3; k++) {
+            for (int l = D; l < D + 3; l++) {
+
+                if (board[k][l] == board[i][j] && board[k][l] != '.') {
+                    count++;
+
+                    if (count > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+
         return true;
     }
 }
