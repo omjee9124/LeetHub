4802859ce58aa9e3e90d6393676205ca06490ea7
Om Ji Dubey <omjeed0666@gmail.com>
2026-09-16 01:59:10 +0530

    Time: 1 ms (99.46%), Space: 46.5 MB (94.77%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,20 +1,29 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int nums[] = new int[arr.length];
-        int k =0;
-        for(int i =0; i< arr.length; i++){
-            if(k>= arr.length) break;
+        int n = arr.length;
+        int zeroes =0;
+        for(int i = 0; i < n;i++){
             if(arr[i] == 0){
-                nums[k++] = 0;
-                if(k>= arr.length) break;
-                nums[k++] = 0;
-            }else{
-                nums[k++] = arr[i];
+                zeroes++;
             }
         }
 
-        for(int i = 0; i<arr.length; i++){
-            arr[i] = nums[i];
+        int j = n + zeroes - 1;
+        int i = n-1;
+
+        while(i>=0){
+            if(j < n){
+                arr[j] = arr[i];
+            }
+            
+
+            if(arr[i] == 0){
+                j--;
+                if(j<n) arr[j] =0;
+            }
+            i--;
+            j--;
         }
+
     }
 }
