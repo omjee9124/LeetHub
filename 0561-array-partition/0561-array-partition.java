f3c2c352a3508b09afa64f48f3538d15a57c7c07
Om Ji Dubey <omjeed0666@gmail.com>
2026-09-23 13:19:35 +0530

    Time: 5 ms (99.85%), Space: 48.2 MB (99.06%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,9 +1,19 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int sum =0;
-        for(int i =0;i< nums.length; i+=2){
-            sum+= nums[i];
+        int k =10000;
+        int countarr[] = new int[2*k +1];
+        for(int i =0; i< nums.length; i++){
+            countarr[nums[i] +k]++;
+        }
+
+        boolean isEven = true;
+        int sum = 0;
+        for(int i = 0; i< 2*k+1; i++){
+            while(countarr[i] > 0){
+                sum = sum + (isEven? i-k : 0);
+                countarr[i]--;
+                isEven = !isEven;
+            }
         }
         return sum;
     }
