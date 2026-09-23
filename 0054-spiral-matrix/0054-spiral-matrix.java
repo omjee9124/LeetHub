c9b666c366d9c7c09c49422bfdadeab439c99d0c
Om Ji Dubey <omjeed0666@gmail.com>
2026-09-23 15:03:46 +0530

    Time: 0 ms (100%), Space: 43 MB (43.88%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,50 +1,27 @@
 class Solution {
-    public static List spiralOrder(int[][] nums) {
-         ArrayList<Integer> ans = new ArrayList<>();
-        int start_row = 0;
-        int start_col = 0;
-        int end_row = nums.length-1;
-        int end_col = nums[0].length-1;
-        int elements = nums[0].length*nums.length;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0;
+        int j = -1;
+        int dir = 1;
 
-        while(elements>0){
-            for(int i = start_col ; i<= end_col ; i++){
-                ans.add(nums[start_row][i]);
-                elements--;
+        while(m>0 && n>0){
+            for(int k = 0; k<n;k++){
+                j+=dir;
+                list.add(matrix[i][j]);
             }
-            start_row++;
-            if(elements <= 0){
-                break;
-            }
+            m--;
 
-            for(int i = start_row; i<=end_row ; i++){
-                ans.add(nums[i][end_col]);
-                elements--;
+            for(int k = 0; k<m;k++){
+                i+=dir;
+                list.add(matrix[i][j]);
             }
-            end_col--;
-            if(elements <= 0){
-                break;
-            }
+            n--;
+            dir = dir * -1;
 
-            for(int i = end_col; i>=start_col ; i--){
-                ans.add(nums[end_row][i]);
-                elements--;
-            }
-            end_row--;
-            if(elements <= 0){
-                break;
-            }
-
-            for(int i = end_row; i >= start_row; i--){
-                ans.add(nums[i][start_col]);
-                elements--;
-            }
-            start_col++;
-            if(elements <= 0){
-                break;
-            }
         }
-        return ans;
-
+        return list;
     }
 }
